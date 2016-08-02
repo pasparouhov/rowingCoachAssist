41109dd58af26d8788da5e637788b73b0735502e
Pavel Asparouhov <pavelasparouhov@bus-067383.local>
2016-08-02 14:45:40 -0700

    First commit weather has okay ui icons working no logic

---
+++ Coach Assist/RealmHelper.swift
@@ -0,0 +1,37 @@
+//
+//  RealmHelper.swift
+//  Coach Assist
+//
+//  Created by Pavel Asparouhov on 8/2/16.
+//  Copyright © 2016 BigP inc. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmHelper {
+    /*static func addNote(note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.add(note)
+        }
+    }
+    static func deleteNote(note: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            realm.delete(note)
+        }
+    }
+    static func retrieveNotes() -> Results<Note> {
+        let realm = try! Realm()
+        return realm.objects(Note).sorted("modificationTime", ascending: false)
+    }
+    static func updateNote(noteToBeUpdated: Note, newNote: Note) {
+        let realm = try! Realm()
+        try! realm.write() {
+            noteToBeUpdated.title = newNote.title
+            noteToBeUpdated.content = newNote.content
+            noteToBeUpdated.modificationTime = newNote.modificationTime
+        }
+    }*/
+}
