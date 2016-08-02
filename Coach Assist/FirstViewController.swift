8f59e95abadf40c9ccd191abfe583790ea5ce21b
Pavel Asparouhov <pavelasparouhov@bus-067383.local>
2016-08-02 10:09:46 -0700

    Initial Commit

---
+++ Coach Assist/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Coach Assist
+//
+//  Created by Pavel Asparouhov on 8/2/16.
+//  Copyright © 2016 BigP inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
